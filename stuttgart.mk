e9b943797a1583fae97e6abc30cc8e93686a7004
scue <linkscue@gmail.com>
2013-06-29 21:06:51 +0800

    stuttgart: fix adb debugging, remove persist.sys.usb.config

--- stuttgart.mk
+++ stuttgart.mk
@@ -327,8 +327,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.allow.mock.location=1 \
-    ro.debuggable=1 \
-	persist.service.adb.enable=1 
+    ro.debuggable=1 
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
