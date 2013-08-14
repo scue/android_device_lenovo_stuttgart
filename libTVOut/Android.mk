ac519ab38f50563cd3aadef5d5673c2120be7959
scue <linkscue@gmail.com>
2013-08-14 10:40:02 +0800

    add missing files: boot_test.sh, libTVOut/, recovery/, rootdir/

---
+++ libTVOut/Android.mk
@@ -0,0 +1,16 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),stuttgart)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libTVOut
+LOCAL_MODULE_OWNER := samsung
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_PATH  := $(TARGET_OUT)/lib
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES    := libTVOut.so
+include $(BUILD_PREBUILT)
+
+endif
