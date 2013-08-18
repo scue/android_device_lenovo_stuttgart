0750061ab6a0d590389280958e6ce9a49eebfef3
scue <linkscue@gmail.com>
2013-08-18 14:14:55 +0800

    BoardConfig.mk: fix I/O Schedular

--- configs/init.lenovo.sh
+++ configs/init.lenovo.sh
@@ -26,6 +26,10 @@ busybox chmod ug+s /system/xbin/su
 busybox chown system:system /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 busybox chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
+#fix I/O Scheduler
+buysbox chown system:system /sys/block/mmcblk0/queue/scheduler
+buysbox chmod 0664 /sys/block/mmcblk0/queue/scheduler
+
 #for preload SuperCam & other
 preload_app_sh=/preload/LeApps/leapp_init.sh
 if [[ -f "$preload_app_sh" ]]; then
