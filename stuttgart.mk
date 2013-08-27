290e241630250609f57e0125b70b580122b1377b
scue <linkscue@gmail.com>
2013-08-28 03:07:23 +0800

    stuttgart.mk: update ro.build.display.id
    Signed-off-by: scue <linkscue@gmail.com>

--- stuttgart.mk
+++ stuttgart.mk
@@ -362,7 +362,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mass_storage \
-    ro.build.display.id=lephone.cc_linkscue_cm10.1 
+    ro.build.display.id=lephone.cc_linkscue_mk4.2.2
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.allow.mock.location=1 \
